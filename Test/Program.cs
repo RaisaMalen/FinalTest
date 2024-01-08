bdf79936673feb07257a69ee64e91fa9bd43e0ca
RaisaMalen <149531386+RaisaMalen@users.noreply.github.com>
2024-01-08 21:35:54 +0300

    Add files via upload

---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿string [] firstArray = new string [5] {"home", "you", "hi", "12345", "win"};
+string [] secondArray = new string [firstArray.Length];
+
+void CreateSecondArray (string [] firstArray, string [] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+    if(firstArray [i].Length <= 3)
+        {
+        secondArray [count] = firstArray[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+CreateSecondArray (firstArray, secondArray);
+PrintArray (secondArray);
